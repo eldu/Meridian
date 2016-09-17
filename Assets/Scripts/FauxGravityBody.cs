a904e74c3e77e92c6b15724eb292875d0083b550
eldu <eldu@seas.upenn.edu>
2016-09-17 13:07:22 -0400

    Alpha

---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityBody : MonoBehaviour {
+	public FauxGravityAttractor attractor;
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().constraints = RigidbodyConstraints.FreezeAll;
+		GetComponent<Rigidbody>().useGravity = false;
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		attractor.Attract (myTransform);
+	}
+}
